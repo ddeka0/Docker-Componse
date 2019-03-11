c1166d661a61b0ed7c8a97b37b00a67e7eea5dde
ddeka0 <dekadebashish3@gmail.com>
2019-03-11 20:28:28 +0530

    Docker compose added
    Added Dockerfile in each of the /team-x directory
    Added Docker-compose file in the root directory
    
    query from postman : POST request at URL "http://localhost:9091/students"
    response:
    ["Debashish","Dhananjay","Lokesh","Parth","Sayali","Heaven","Priti","Sourabh","Karan","Gaurav","Dhaval","Avais"]
    
    status: working

--- team1/src/controllers/StudentController.js
+++ team1/src/controllers/StudentController.js
@@ -1,16 +1,19 @@
-var request = require("request");
+var Request = require("request");
 module.exports = {
 	students: async function (req, res) {
 		/* There are the names which belongs to Team2 server */
+
 		var names = ["Debashish", "Dhananjay", "Lokesh","Parth", "Sayali", "Heaven"];
 		
 		/*fetch the other names from the Team2 server */
-		request.post("http://localhost:8082/students", function (error, response, body) {		
+		Request.post("http://team2:8082/students", function (error, response, body) {		
 			if(!error) {
 				let names2 = JSON.parse(body)
 				for(let i = 0, l = names2.length; i < l; i++) {
 					names.push(names2[i])
 				}
+			}else {
+				console.log(error)
 			}
 			try {
 				res.send(JSON.stringify(names))
